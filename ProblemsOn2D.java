c9ba831a0257bc18e32b42c50d030429b2ac9fec
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-06-09 11:04:55 +0530

    Problems on 2D-Array

--- ProblemsOn2D.java
+++ ProblemsOn2D.java
@@ -29,3 +29,29 @@ class AdditionOf2Matrices{
         sc.close();
     }
 }
+
+/*Program to replace a value from MxN matrix */
+
+class replaceValue{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt(), N = sc.nextInt();
+        int[][] arr1 = new int[M][N];
+        for(int i=0; i<M;i++){
+            for(int j=0;j<N;j++){
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+        int a=sc.nextInt(), b = sc.nextInt();
+        for(int i=0; i<M;i++){
+            for(int j=0;j<N;j++){
+                if(arr1[i][j]==a){
+                    arr1[i][j]=b;
+                }
+                System.out.print(arr1[i][j]+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
