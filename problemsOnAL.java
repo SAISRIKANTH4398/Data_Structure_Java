f6a04beb1269169aabd5badbc999d420b7d0164b
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-07-03 08:36:27 +0530

    Problems on Arrays and ArrayLists

--- problemsOnAL.java
+++ problemsOnAL.java
@@ -31,3 +31,41 @@ class Main11{
         sc.close();
     }
 }
+
+
+class Main12{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int[][] arr = new int[M][N];
+        int[] maxArr = new int[M];
+        int[] minArr = new int[M];
+        int[] sumArr = new int[M];
+        for(int i=0;i<M;i++){
+            for(int j=0;j<N;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int k=0;k<M;k++){
+            int maxNum = arr[k][0];
+            int minNum = arr[k][0];
+            int sum = 0;
+            for(int l=0;l<N;l++){
+                if(maxNum<=arr[k][l]){
+                    maxNum = arr[k][l];
+                }
+                minNum = minNum>=arr[k][l]?arr[k][l]:minNum;
+                sum+=arr[k][l];
+            }
+            maxArr[k] = maxNum;
+            minArr[k] = minNum;
+            sumArr[k] = sum;
+        }
+        System.out.println(Arrays.toString(maxArr));
+        System.out.println(Arrays.toString(minArr));
+        System.out.println(Arrays.toString(sumArr));
+
+        sc.close();
+    }
+}
