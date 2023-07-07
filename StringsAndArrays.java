163f62dc39c63ce4be7ed6cd00c4fe1fcde39434
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-07-07 10:20:40 +0530

    Strings and Arrays

--- StringsAndArrays.java
+++ StringsAndArrays.java
@@ -8,3 +8,26 @@ class SplittingStrings {
         System.out.println(Arrays.toString(messageArr)); //["See", "you", "soon"]
     }
 }
+
+/*
+ * Regex or Regular Expression is a sequence of characters that define a pattern that can be used to search for or manipulate strings.
+ * metacharacter: dot (.)
+   bracket list: []
+   position anchors: ^, $
+   occurrence indicators: +, *, ?, { }
+   parentheses: ()
+   or: |
+   escape and metacharacter: backslash (\)
+ */
+
+ class splitWithRegex{
+    public static void main(String[] args) {
+        String str = "One1Two22Three333Four";
+        String[] stringArr = str.split("[0-9]+"); 
+        /*The Regex pattern [.-.] accepts any one of the characters in the range, and 
+        the + character means to find one or more occurrences of the given character or a regex pattern. */
+        System.out.println(Arrays.toString(stringArr)); //[One, Two, Three, Four].
+
+        
+    }
+ }
