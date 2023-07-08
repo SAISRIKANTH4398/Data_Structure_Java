b4527d6b37e73a7b3d75162c60461eeb5d9fe365
SAISRIKANTH4398 <sreekanthkasavajjula@gmail.com>
2023-07-08 22:52:31 +0530

    HashSets

---
+++ HashSets.java
@@ -0,0 +1,17 @@
+/*Hashsets are unordered collection of elements. It allows unique values, repeated values are not allowed.
+ * HashSets are from java.util package.
+ * They store only objects like String, Integer..
+ * 
+ * Syntax:-  HashSet<Type> var = new HashSet<>();
+ */
+
+import java.util.HashSet;
+
+class Main14 {
+    public static void main(String[] args) {
+        HashSet<String> players = new HashSet<>();
+        players.add("Rohit");
+        players.add("Virat");
+        players.add("Dhoni");  //To add an element to a HashSet.
+    }
+}
